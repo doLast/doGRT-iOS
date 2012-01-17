c8d24a4d6a078f4dae63e887bf237537a05d3f12
Greg Wang <gregwang@localhost.(none)>
2012-01-17 18:14:14 +0800

    Initial Commit

---
+++ doGRT/GRTAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  GRTAppDelegate.h
+//  doGRT
+//
+//  Created by Greg Wang on 12-1-17.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GRTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ doGRT/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  doGRT
+//
+//  Created by Greg Wang on 12-1-17.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GRTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([GRTAppDelegate class]));
+	}
+}
