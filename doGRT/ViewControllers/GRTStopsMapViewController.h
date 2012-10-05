22ecb62c1a2e3ac200932822b698289bc7809f89
Greg Wang <gregwym@qq.com>
2012-10-04 22:54:59 -0400

    Add StopsMapViewController which is responsible for controlling the map view

---
+++ doGRT/ViewControllers/GRTStopsMapViewController.h
@@ -0,0 +1,32 @@
+//
+//  GRTStopsMapViewController.h
+//  doGRT
+//
+//  Created by Greg Wang on 12-10-4.
+//
+//
+
+#import <MapKit/MapKit.h>
+#import "GRTGtfsSystem.h"
+
+@class GRTStopsMapViewController;
+
+@protocol GRTStopsMapViewControllerDelegate <NSObject>
+
+- (void)mapViewController:(GRTStopsMapViewController *)mapViewController didSelectStop:(GRTStop *)stop;
+- (void)mapViewController:(GRTStopsMapViewController *)mapViewController didUpdateUserLocation:(MKUserLocation *)userLocation;
+
+@end
+
+@interface GRTStopsMapViewController : UIViewController <MKMapViewDelegate>
+
+@property (nonatomic, weak) id<GRTStopsMapViewControllerDelegate> delegate;
+@property (nonatomic, weak) IBOutlet MKMapView *mapView;
+@property (nonatomic, strong) NSArray *stops;
+
+- (void)setUserTrackingMode:(MKUserTrackingMode)mode animated:(BOOL)animated;
+- (void)centerMapToRegion:(MKCoordinateRegion)region animated:(BOOL)animated;
+- (void)setMapAlpha:(CGFloat)alpha animationDuration:(NSTimeInterval)duration;
+- (void)selectStop:(id<GRTStopAnnotation>)annotation;
+
+@end
---
+++ doGRT/ViewControllers/GRTStopsMapViewController.m
@@ -0,0 +1,198 @@
+//
+//  GRTStopsMapViewController.m
+//  doGRT
+//
+//  Created by Greg Wang on 12-10-4.
+//
+//
+
+#import "GRTStopsMapViewController.h"
+#import "GRTGtfsSystem.h"
+#import "GRTUserProfile.h"
+
+@interface GRTStopsMapViewController ()
+
+@property (nonatomic, weak) id<GRTStopAnnotation> willBePresentedStop;
+@property (nonatomic, strong, readonly) NSOperationQueue *annotationUpdateQueue;
+
+@end
+
+@implementation GRTStopsMapViewController
+
+@synthesize delegate = _delegate;
+@synthesize mapView = _mapView;
+@synthesize stops = _stops;
+@synthesize willBePresentedStop = _willBePresentedStop;
+@synthesize annotationUpdateQueue = _annotationUpdateQueue;
+
+- (void)setStops:(NSArray *)stops
+{
+	NSMutableArray *toRemove = [[self.mapView.annotations filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"self isKindOfClass: %@", [GRTFavoriteStop class]]] mutableCopy];
+	NSMutableArray *toAdd = [stops mutableCopy];
+	[toAdd removeObjectsInArray:toRemove];
+	[toRemove removeObjectsInArray:stops];
+	
+	NSLog(@"Adding: %@, Removing: %@", toAdd, toRemove);
+	
+	dispatch_async(dispatch_get_main_queue(), ^{
+		[self.mapView removeAnnotations:toRemove];
+		[self.mapView addAnnotations:toAdd];
+		[self updateMapView];
+	});
+}
+
+- (NSOperationQueue *)annotationUpdateQueue
+{
+	if (_annotationUpdateQueue == nil) {
+		_annotationUpdateQueue = [[NSOperationQueue alloc] init];
+	}
+	return _annotationUpdateQueue;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+	
+	// Center Waterloo on map
+	[self centerMapToRegion:MKCoordinateRegionMakeWithDistance(CLLocationCoordinate2DMake(43.47273, -80.541218), 2000, 2000) animated:NO];
+}
+
+#pragma mark - view update
+
+- (void)updateMapView
+{
+	// If invisible, do nothing
+	if (self.mapView.alpha == 0) {
+		return;
+	}
+	
+	NSOperation *annotationUpdate = [[NSInvocationOperation alloc] initWithTarget:self selector:@selector(performAnnotationUpdate) object:nil];
+	
+	[self.annotationUpdateQueue addOperation:annotationUpdate];
+}
+
+- (void)performAnnotationUpdate
+{
+	@synchronized(self.mapView) {
+		MKCoordinateRegion region = self.mapView.region;
+		
+		// find out all need to remove annotations
+		NSSet *visibleAnnotations = [self.mapView annotationsInMapRect:[self.mapView visibleMapRect]];
+		NSSet *allAnnotations = [NSSet setWithArray:self.mapView.annotations];
+		NSMutableSet *nonVisibleAnnotations = [NSMutableSet setWithSet:allAnnotations];
+		[nonVisibleAnnotations minusSet:visibleAnnotations];
+		[nonVisibleAnnotations filterUsingPredicate:[NSPredicate predicateWithFormat:@"self isKindOfClass: %@", [GRTStop class]]];
+		
+		// Also remove stops overlay on fav stops
+		NSSet *visibleFavAnnotations = [visibleAnnotations filteredSetUsingPredicate:[NSPredicate predicateWithFormat:@"self isKindOfClass: %@", [GRTFavoriteStop class]]];
+		for (GRTFavoriteStop *fav in visibleFavAnnotations) {
+			[nonVisibleAnnotations addObject:fav.stop];
+		}
+		
+		dispatch_async(dispatch_get_main_queue(), ^{
+			[self.mapView removeAnnotations:[nonVisibleAnnotations allObjects]];
+			
+			// if not too many annotations currently on the map
+			if([[self.mapView annotations] count] < 50){
+				// get bus stops in current region
+				NSArray *newStops = [[GRTGtfsSystem defaultGtfsSystem] stopsInRegion:region];
+				
+				NSMutableSet *newAnnotations = [NSMutableSet setWithArray:newStops];
+				[newAnnotations minusSet:visibleAnnotations];
+				
+				[self.mapView addAnnotations:[newAnnotations allObjects]];
+			}
+		});
+	}
+}
+
+#pragma mark - actions
+
+- (void)setUserTrackingMode:(MKUserTrackingMode)mode animated:(BOOL)animated
+{
+	[self.mapView setUserTrackingMode:mode animated:animated];
+}
+
+- (void)centerMapToRegion:(MKCoordinateRegion)region animated:(BOOL)animated
+{
+	if (self.willBePresentedStop != nil) {
+		for (id<MKAnnotation> annotationView in self.mapView.selectedAnnotations) {
+			[self.mapView deselectAnnotation:annotationView animated:NO];
+		}
+		[self.mapView selectAnnotation:self.willBePresentedStop animated:NO];
+	}
+	[self.mapView setRegion:region animated:animated];
+}
+
+- (void)setMapAlpha:(CGFloat)alpha animationDuration:(NSTimeInterval)duration
+{
+	[UIView animateWithDuration:duration animations:^{
+		self.mapView.alpha = alpha;
+	} completion:^(BOOL finished){
+		if (alpha > 0) {
+			[self updateMapView];
+		}
+	}];
+}
+
+- (void)selectStop:(id<GRTStopAnnotation>)annotation
+{
+	self.willBePresentedStop = annotation;
+	[self centerMapToRegion:MKCoordinateRegionMakeWithDistance(annotation.coordinate, 300, 300) animated:NO];
+}
+
+#pragma mark - Map View Delegate
+
+- (void)mapView:(MKMapView *)mapView regionDidChangeAnimated:(BOOL)animated
+{
+	[self updateMapView];
+}
+
+- (void)mapView:(MKMapView *)mapView didSelectAnnotationView:(MKAnnotationView *)view
+{
+	if (view.annotation == self.willBePresentedStop) {
+		self.willBePresentedStop = nil;
+	}
+}
+
+- (void)mapView:(MKMapView *)mapView didAddAnnotationViews:(NSArray *)views
+{
+	for (MKAnnotationView *view in views) {
+		if ([view isKindOfClass:[MKPinAnnotationView class]]) {
+			MKPinAnnotationView *pin = (MKPinAnnotationView *) view;
+			if ([view.annotation isKindOfClass:[GRTFavoriteStop class]]) {
+				pin.pinColor = MKPinAnnotationColorGreen;
+			}
+			else {
+				pin.pinColor = MKPinAnnotationColorRed;
+			}
+			pin.rightCalloutAccessoryView = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+		}
+		if ([view.annotation isKindOfClass:[GRTStop class]] && [[GRTUserProfile defaultUserProfile] favoriteStopByStop:(GRTStop *)view.annotation] != nil) {
+			[mapView removeAnnotation:view.annotation];
+		}
+	}
+	if (self.willBePresentedStop != nil && [mapView.selectedAnnotations count] == 0) {
+		[mapView selectAnnotation:self.willBePresentedStop animated:NO];
+	}
+}
+
+- (void)mapView:(MKMapView *)mapView annotationView:(MKAnnotationView *)view calloutAccessoryControlTapped:(UIControl *)control
+{
+	if (self.delegate != nil && [view.annotation respondsToSelector:@selector(stop)]) {
+		GRTStop *stop = [((id<GRTStopAnnotation>) view.annotation) stop];
+		if (stop != nil && [self.delegate respondsToSelector:@selector(mapViewController:didSelectStop:)]) {
+			[self.delegate mapViewController:self didSelectStop:stop];
+		}
+	}
+}
+
+- (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation
+{
+	if (self.delegate != nil && [self.delegate respondsToSelector:@selector(mapViewController:didUpdateUserLocation:)]) {
+		[self.delegate mapViewController:self didUpdateUserLocation:userLocation];
+	}
+}
+
+@end
