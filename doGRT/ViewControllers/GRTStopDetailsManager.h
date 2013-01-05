02735675db22815c8ef93432a0eadf8be94ed28c
Greg Wang <gregwym@qq.com>
2013-01-05 13:12:51 -0500

    Add StopDetailsManager to manage the current selection of day/date/route for a stopDetails

---
+++ doGRT/ViewControllers/GRTStopDetailsManager.h
@@ -0,0 +1,32 @@
+//
+//  GRTStopDetailsManager.h
+//  doGRT
+//
+//  Created by Greg Wang on 13-1-5.
+//
+//
+
+#import "PopoverView.h"
+@class GRTStopDetails;
+@class GRTRoute;
+
+@protocol GRTStopDetailsManagerDelegate <NSObject>
+
+@property (readwrite) NSArray *stopTimes;
+- (UIView *)view;
+- (UINavigationItem *)navigationItem;
+
+@end
+
+@interface GRTStopDetailsManager : NSObject <PopoverViewDelegate>
+
+@property (nonatomic, strong) GRTStopDetails *stopDetails;
+@property (nonatomic, strong) GRTRoute *route;
+@property (nonatomic) NSUInteger dayInWeek;
+@property (nonatomic, strong) NSDate *date;
+@property (nonatomic, weak) id<GRTStopDetailsManagerDelegate> delegate;
+
+- (GRTStopDetailsManager *)initWithStopDetails:(GRTStopDetails *)stopDetails;
+- (GRTStopDetailsManager *)initWithStopDetails:(GRTStopDetails *)stopDetails route:(GRTRoute *)route;
+
+@end
---
+++ doGRT/ViewControllers/GRTStopDetailsManager.m
@@ -0,0 +1,146 @@
+//
+//  GRTStopDetailsManager.m
+//  doGRT
+//
+//  Created by Greg Wang on 13-1-5.
+//
+//
+
+#import "GRTDetailedTitleButtonView.h"
+
+#import "GRTStopDetailsManager.h"
+#import "GRTGtfsSystem.h"
+
+@interface GRTStopDetailsManager ()
+
+@property (nonatomic, weak) GRTDetailedTitleButtonView *stopDetailsTitleView;
+
+@end
+
+@implementation GRTStopDetailsManager
+
+@synthesize stopDetails = _stopDetails;
+@synthesize route = _route;
+@synthesize dayInWeek = _dayInWeek;
+@synthesize date = _date;
+@synthesize delegate = _delegate;
+
+@synthesize stopDetailsTitleView = _stopDetailsTitleView;
+
+- (void)setDelegate:(id<GRTStopDetailsManagerDelegate>)delegate
+{
+	_delegate = delegate;
+	if (delegate != nil) {
+		self.stopDetailsTitleView = [self constructTitleView];
+		[self updateStopTimes];
+	}
+}
+
+- (void)setStopDetailsTitleView:(GRTDetailedTitleButtonView *)stopDetailsTitleView
+{
+	_stopDetailsTitleView = stopDetailsTitleView;
+	self.delegate.navigationItem.titleView = stopDetailsTitleView;
+}
+
+#pragma mark - constructors
+
+- (GRTStopDetailsManager *)initWithStopDetails:(GRTStopDetails *)stopDetails
+{
+	self = [self initWithStopDetails:stopDetails route:nil];
+	return self;
+}
+
+- (GRTStopDetailsManager *)initWithStopDetails:(GRTStopDetails *)stopDetails route:(GRTRoute *)route
+{
+	if (stopDetails == nil) {
+		return nil;
+	}
+	self = [super init];
+	if (self != nil) {
+		self.stopDetails = stopDetails;
+		self.route = route;
+		self.dayInWeek = 1;
+		self.date = [NSDate date];
+	}
+	return self;
+}
+
+- (GRTDetailedTitleButtonView *)constructTitleView
+{
+	NSString *title = self.stopDetails.stop.stopName;
+	if (self.route != nil) {
+		title = [title stringByAppendingFormat:@" - %d", self.route.routeId.integerValue];
+	}
+	GRTDetailedTitleButtonView *titleView = [[GRTDetailedTitleButtonView alloc] initWithText:title detailText:@"Today ▾"];
+	[titleView addTarget:self action:@selector(showModePicker:) forControlEvents:UIControlEventTouchUpInside];
+	return titleView;
+}
+
+#pragma mark - actions
+
+- (void)updateStopTimes
+{
+	NSArray *stopTimes = nil;
+	if (self.date == nil) {
+		stopTimes = [self.stopDetails stopTimesForDayInWeek:self.dayInWeek andRoute:self.route];
+	}
+	else {
+		stopTimes = [self.stopDetails stopTimesForDate:self.date andRoute:self.route];
+	}
+	self.delegate.stopTimes = stopTimes;
+}
+
+- (IBAction)showModePicker:(id)sender
+{
+	CGPoint point = CGPointMake(self.delegate.view.frame.size.width / 2.0, 0.0f);
+	PopoverView *popoverView = [PopoverView showPopoverAtPoint:point inView:self.delegate.view withStringArray:[NSArray arrayWithObjects:@"Today", @"Day in a week", /*@"Certain Date", */nil] delegate:self];
+	popoverView.tag = 0;
+}
+
+- (IBAction)showDayPicker:(id)sender
+{
+	CGPoint point = CGPointMake(self.delegate.view.frame.size.width / 2.0, 0.0f);
+	PopoverView *popoverView = [PopoverView showPopoverAtPoint:point inView:self.delegate.view withTitle:@"Pick a date" withStringArray:@[@"Sunday/Holiday", @"Monday", @"Tuesday", @"Wednesday", @"Thursday", @"Friday", @"Saturday"] delegate:self];
+	popoverView.tag = 1;
+}
+
+- (IBAction)showDatePicker:(id)sender
+{
+	CGPoint point = CGPointMake(self.delegate.view.frame.size.width / 2.0, 0.0f);
+	// TODO: Use Calendar Instead
+	PopoverView *popoverView = [PopoverView showPopoverAtPoint:point inView:self.delegate.view withTitle:@"Pick a date" withStringArray:@[@"Sunday/Holiday", @"Monday", @"Tuesday", @"Wednesday", @"Thursday", @"Friday", @"Saturday"] delegate:self];
+	popoverView.tag = 2;
+}
+
+#pragma mark - popover view delegate
+
+- (void)popoverView:(PopoverView *)popoverView didSelectItemAtIndex:(NSInteger)index
+{
+	NSUInteger tag = popoverView.tag;
+	[popoverView dismiss];
+	if (tag == 0) {
+		switch (index) {
+			case 1:
+				[self showDayPicker:self];
+				break;
+			case 2:
+				[self showDatePicker:self];
+				break;
+			default:
+				self.date = [NSDate date];
+				self.stopDetailsTitleView.detailTextLabel.text = @"Today ▾";
+				break;
+		}
+	}
+	else if (tag == 1) {
+		self.dayInWeek = index + 1;
+		self.date = nil;
+		NSArray *days = @[@"Sunday/Holiday", @"Monday", @"Tuesday", @"Wednesday", @"Thursday", @"Friday", @"Saturday"];
+		self.stopDetailsTitleView.detailTextLabel.text = [NSString stringWithFormat:@"%@ ▾", [days objectAtIndex:index]];
+	}
+	// TODO: Handle other type of popovers
+	
+	[self updateStopTimes];
+}
+
+@end
