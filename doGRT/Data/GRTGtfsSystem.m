549426e4f7066061b6464647dce5be14e5c4975d
Greg Wang <gregwym@gmail.com>
2014-08-31 23:19:39 -0400

    Update schedule data with 20140831 version.

--- doGRT/Data/GRTGtfsSystem.m
+++ doGRT/Data/GRTGtfsSystem.m
@@ -12,8 +12,8 @@
 #import "ASIHTTPRequest.h"
 
 static const NSInteger kMaxStopsLimit = 30;
-static const NSInteger kBuiltInDataVersion = 20140621;
-static const NSInteger kBuiltInDataEndDate = 20140831;
+static const NSInteger kBuiltInDataVersion = 20140831;
+static const NSInteger kBuiltInDataEndDate = 20141221;
 
 NSString * const GRTGtfsDataVersionKey = @"GRTGtfsDataVersionKey";
 NSString * const GRTGtfsDataEndDateKey = @"GRTGtfsDataEndDateKey";
